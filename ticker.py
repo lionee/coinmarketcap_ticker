1504209e275c2a66a71fab41a2dc51e08cf0dacb
Daniel <czerniawski.daniel@gmail.com>
2025-01-19 20:41:41 +0100

    Added some error handling

--- ticker.py
+++ ticker.py
@@ -4,7 +4,7 @@
 # ... and presented it as small neat bar, which can be placed anywhere on screen and stopped by right click.
 # Daniel Czerniawski 06.01.2022
 
-import locale
+
 import PySimpleGUI as sg
 from requests import Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
@@ -37,14 +37,14 @@ def get_data(slug):
 
     except (ConnectionError, Timeout, TooManyRedirects) as e:
         print(e)
+
+
+
     return round(price, 6), rank
 
 if __name__ == '__main__':
-
-
     # Read config from config.cfg
     # Put your API-KEY there
-
     config = configparser.ConfigParser()
     try:
         config.read('config.cfg')
@@ -55,14 +55,7 @@ if __name__ == '__main__':
         print(f"Configuration error: {e}")
         exit(1)
 
-    locale.setlocale(locale.LC_ALL, 'en_US')
-
-    # Get screen size
-    w, h = sg.Window.get_screen_size()
-
-    text1 = slug.upper() + ": "
-    text2 = "$" + str(get_data(slug)[0])
-    text3 = " #" + str(get_data(slug)[1])
+    text1, text2, text3 = "", "Connecting ...", ""
 
     # Set layout for window
     # Just simple one line without any buttons
@@ -71,6 +64,8 @@ if __name__ == '__main__':
          sg.Text(text2, key='price', pad=(0, 0), font=('Arial', 10, 'bold'), background_color='#333333'),
          sg.Text(text3, key='coinmarketcap_position', pad=(0, 0), font=('Arial', 10, 'bold'), background_color='#333333')]
     ]
+    # Get screen size
+    w, h = sg.Window.get_screen_size()
 
     # Put window on top of the screen in middle
     # No title bar, always on top
@@ -80,46 +75,58 @@ if __name__ == '__main__':
     window['price'].bind('<Button-3>', '+RIGHT CLICK+')
     window['coinmarketcap_position'].bind('<Button-3>', '+RIGHT CLICK+')
 
-    data = get_data(slug)
-    last_price, last_pos = data
-
-
+    event, values = window.read(timeout=1000)
 
     # Main loop
     while True:
-
-        event, values = window.read(timeout=refresh_rate)
-
-        data = get_data(slug)
-        current_price, current_pos = data
-
-        text2 = "$" + str(current_price)
-        text3 = " #" + str(current_pos)
-
+        try:
+            data = get_data(slug)
+            current_price, current_pos = data
+        except:
+            window['price'].update(text_color='#FF5555')
+            window['coinmarketcap_position'].update(text_color='#FF5555')
+            data = ("Cannot connect", "to coinmarketcap API!")
+            last_price, last_pos = data
+            current_price, current_pos = data
+            text1 = ""
+            text2 = last_price
+            text3 = last_pos
+            pass
+        else:
+            text1 = slug.upper() + ": "
+            text2 = "$" + str(current_price)
+            text3 = " #" + str(current_pos)
+        window['currency_name'].update(text1)
         window['price'].update(text2)
         window['coinmarketcap_position'].update(text3)
 
         # Change color to red if price is lower and to green if price is higher
-        if float(last_price) < float(current_price):
-            window['price'].update(text_color='#55FF55')
-        elif float(last_price) > float(current_price):
-            window['price'].update(text_color='#FF5555')
-        elif float(last_price) == float(current_price):
-            window['price'].update(text_color='#EEEEEE')
+        try:
+            if float(last_price) < float(current_price):
+                window['price'].update(text_color='#55FF55')
+            elif float(last_price) > float(current_price):
+                window['price'].update(text_color='#FF5555')
+            elif float(last_price) == float(current_price):
+                window['price'].update(text_color='#EEEEEE')
+        except:
+            pass
 
         # Change color to red if position is lower and to green if price is higher
-        if float(last_pos) < float(current_pos):
-            window['coinmarketcap_position'].update(text_color='#FF5555')
-        elif float(last_pos) > float(current_pos):
-            window['coinmarketcap_position'].update(text_color='#55FF55')
-        elif float(last_pos) == float(current_pos):
-            window['coinmarketcap_position'].update(text_color='#EEEEEE')
+        try:
+            if float(last_pos) < float(current_pos):
+                window['coinmarketcap_position'].update(text_color='#FF5555')
+            elif float(last_pos) > float(current_pos):
+                window['coinmarketcap_position'].update(text_color='#55FF55')
+            elif float(last_pos) == float(current_pos):
+                window['coinmarketcap_position'].update(text_color='#EEEEEE')
+        except:
+            pass
 
         last_price = current_price
         last_pos = current_pos
 
         if event == "currency_name+RIGHT CLICK+" or event == "price+RIGHT CLICK+" or event == "coinmarketcap_position+RIGHT CLICK+" or event == sg.WIN_CLOSED:
             break
-
+        event, values = window.read(timeout=refresh_rate)
 
     window.close()
