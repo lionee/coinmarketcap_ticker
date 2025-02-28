9ec7137cc174df55f6e6de0de9df8bd9276c258d
lione <czerniawski.daniel@gmail.com>
2025-02-28 07:35:06 +0100

    Added support for multiple cryptocurencies.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # coinmarketcap_ticker
 Coinmarketcap ticker
 is app to gather data from coinmarketcap for selected cryptocurrency and presenting it as small neat bar, which can be placed anywhere on screen (left mouse drag) and stopped by right click. It support colors.
- 
+You may add multiple cryptocurrencies in config file to stack bars
+
 You need to generate API key on coinmarketcap and put it in config.cfg
  
 Bar shows info like below:
--- ticker.py
+++ ticker.py
@@ -3,7 +3,7 @@
 # App to gather data from coinmarketcap for selected cryptocurrency...
 # ... and presented it as small neat bar, which can be placed anywhere on screen and stopped by right click.
 # Daniel Czerniawski 06.01.2022
-
+import time
 
 import PySimpleGUI as sg
 from requests import Session
@@ -31,15 +31,11 @@ def get_data(slug):
         response = session.get(url, params=parameters)
         data = json.loads(response.text)
         k = list(data['data'].keys())
-        #pprint.pprint(data['data'])
         rank = data['data'][k[0]]['cmc_rank']
         price = data['data'][k[0]]['quote']['USD']['price']
 
     except (ConnectionError, Timeout, TooManyRedirects) as e:
         print(e)
-
-
-
     return round(price, 6), rank
 
 def switch_colors(last, current, field):
@@ -63,8 +59,9 @@ if __name__ == '__main__':
         api_key = config['DEFAULT']['api_key']
         slug = config['DEFAULT']['currency']
         refresh_rate = config['DEFAULT']['refresh_rate']
-        amount = float(config['DEFAULT']['amount'])
+        amount = config['DEFAULT']['amount']
         background_colour_default = config['DEFAULT']['background_color_default']
+        font_size = config['DEFAULT']['font_size']
         font_colour_default = config['DEFAULT']['font_color_default']
         font_colour_better = config['DEFAULT']['font_color_better']
         font_colour_worse = config['DEFAULT']['font_color_worse']
@@ -73,16 +70,47 @@ if __name__ == '__main__':
         print(f"Configuration error: {e}")
         exit(1)
 
-    text1, text2, text3, text4 = "", "Connecting ...", "", ""
 
-    # Set layout for window
-    # Just simple one line without any buttons
-    layout = [
-        [sg.Text(text1, key='currency_name', pad=(0, 0), font=('Arial', 10, 'bold'), background_color='#'+str(background_colour_default)),
-         sg.Text(text2, key='price', pad=(0, 0), font=('Arial', 10, 'bold'), background_color='#'+str(background_colour_default)),
-         sg.Text(text3, key='coinmarketcap_position', pad=(0, 0), font=('Arial', 10, 'bold'), background_color='#'+str(background_colour_default)),
-         sg.Text(text4, key='current_amount_value', pad=(0, 0), font=('Arial', 10, 'bold'), background_color='#'+str(background_colour_default))]
-    ]
+    slugs = [x.strip() for x in slug.split(',')]
+    slugs_amounts = [x.strip() for x in amount.split(',')]
+
+    text1, text2, text3, text4 = "", "Connecting ...", "", ""
+    rows = []
+    layout = []
+    last_price = last_pos = last_value = []
+
+    my_new_theme = {'BACKGROUND': '#'+background_colour_default,
+                    'TEXT': '#'+font_colour_default,
+                    'INPUT': '#c7e78b',
+                    'TEXT_INPUT': '#000000',
+                    'SCROLL': '#c7e78b',
+                    'BUTTON': ('white', '#709053'),
+                    'PROGRESS': ('#01826B', '#D0D0D0'),
+                    'BORDER': 0,
+                    'SLIDER_DEPTH': 0,
+                    'PROGRESS_DEPTH': 0}
+    sg.theme_add_new('CoinmarketcapTickerTheme', my_new_theme)
+
+    # Switch your theme to use the newly added one.
+    sg.theme('CoinmarketcapTickerTheme')
+
+    for x in range(len(slugs)):
+
+        rows.append([
+         sg.Text(text1, key='currency_name_'+slugs[x], pad=(0, 0), font=('Arial', font_size, 'bold'), background_color='#'+str(background_colour_default)),
+         sg.Text(text2, key='price_'+slugs[x], pad=(0, 0), font=('Arial', font_size, 'bold'), background_color='#'+str(background_colour_default)),
+         sg.Text(text3, key='coinmarketcap_position_'+slugs[x], pad=(0, 0), font=('Arial', font_size, 'bold'), background_color='#'+str(background_colour_default)),
+         sg.Text(text4, key='current_amount_value_'+slugs[x], pad=(0, 0), font=('Arial', font_size, 'bold'), background_color='#'+str(background_colour_default))
+        ])
+
+        last_price.append(0)
+        last_pos.append(0)
+        last_value.append(0)
+    layout = ([rows])
+    #layout.append(rows)
+    print(layout)
+
+
     # Get screen size
     w, h = sg.Window.get_screen_size()
 
@@ -90,61 +118,68 @@ if __name__ == '__main__':
     # No title bar, always on top
     # Right mouse click to close app
     window = sg.Window("Coinmarketcap ticker", layout, location=(w/2-100,0), no_titlebar=True, keep_on_top=True, grab_anywhere=True, margins=(0,0), finalize=True)
-    window['currency_name'].bind('<Button-3>', '+RIGHT CLICK+')
-    window['price'].bind('<Button-3>', '+RIGHT CLICK+')
-    window['coinmarketcap_position'].bind('<Button-3>', '+RIGHT CLICK+')
-    window['current_amount_value'].bind('<Button-3>', '+RIGHT CLICK+')
+    window['currency_name_'+slugs[x]].bind('<Button-3>', '+RIGHT CLICK+')
+    window['price_'+slugs[x]].bind('<Button-3>', '+RIGHT CLICK+')
+    window['coinmarketcap_position_'+slugs[x]].bind('<Button-3>', '+RIGHT CLICK+')
+    window['current_amount_value_'+slugs[x]].bind('<Button-3>', '+RIGHT CLICK+')
 
-    event, values = window.read(timeout=1000)
+    event, values = window.read(timeout=5)
 
     # Main loop
     while True:
-        try:
-            data = get_data(slug)
-            current_price, current_pos = data
-        except:
-            window['price'].update(text_color='#'+str(font_colour_worse))
-            window['coinmarketcap_position'].update(text_color='#'+str(font_colour_worse))
-            data = ("Cannot connect", "to coinmarketcap API!")
-            last_price, last_pos = data
-            current_price, current_pos = data
-            text1 = ""
-            text2 = last_price
-            text3 = last_pos
-            text4 = ""
-            pass
-        else:
-            text1 = slug.upper() + ": "
-            text2 = "$" + str(current_price)
-            text3 = " #" + str(current_pos)
-            if int(amount) != 0:
-                #print(amount)
-                current_amount_value = round(float(amount) * float(current_price), 2)
-                text4 = " $" + str(current_amount_value)
-            else:
+        for x in range(len(slugs)):   # Iterating through all slugs defined in config.cfg
+
+            try:
+                data = get_data(slugs[x])
+                current_price, current_pos = data
+
+            except:
+                window['price_'+slugs[x]].update(text_color='#'+str(font_colour_worse))
+                window['coinmarketcap_position_'+slugs[x]].update(text_color='#'+str(font_colour_worse))
+                data = ("Cannot connect", "to coinmarketcap API!")
+                last_price, last_pos = data
+                current_price, current_pos = data
+                text1 = ""
+                text2 = last_price[x]
+                text3 = last_pos[x]
                 text4 = ""
-        window['currency_name'].update(text1)
-        window['price'].update(text2)
-        window['coinmarketcap_position'].update(text3)
-        window['current_amount_value'].update(text4)
+                pass
+            else:
+                text1 = slugs[x].upper() + ": "
+                text2 = "$" + str(current_price)
+                text3 = " #" + str(current_pos)
+                if float(slugs_amounts[x]) != 0:
+                    #print(amount)
+                    current_amount_value = round(float(slugs_amounts[x]) * float(current_price), 2)
+                    text4 = " $" + str(current_amount_value)
+                else:
+                    text4 = ""
+            window['currency_name_'+slugs[x]].update(text1)
+            window['price_'+slugs[x]].update(text2)
+            window['coinmarketcap_position_'+slugs[x]].update(text3)
+            window['current_amount_value_'+slugs[x]].update(text4)
 
 
-        try:
-            switch_colors(last_price, current_price, window['price'])
-            switch_colors(last_pos, current_pos, window['coinmarketcap_position'])
-            switch_colors(last_value, current_amount_value, window['current_amount_value'])
-        except:
-            pass
+            try:
+                switch_colors(last_price[x], current_price, window['price_'+slugs[x]])
+                switch_colors(current_pos, last_pos[x], window['coinmarketcap_position_'+slugs[x]])
+                switch_colors(last_value[x], current_amount_value, window['current_amount_value_'+slugs[x]])
+            except:
+                pass
+
+            last_price[x] = current_price
+            last_pos[x] = current_pos
+            last_value[x] = current_amount_value
+
+            if (event == "currency_name_"+slugs[x]+"+RIGHT CLICK+" or
+                    event == "price_"+slugs[x]+"+RIGHT CLICK+" or
+                    event == "coinmarketcap_position_"+slugs[x]+"+RIGHT CLICK+" or
+                    event == "current_amount_value_"+slugs[x]+"+RIGHT CLICK+" or
+                    event == sg.WIN_CLOSED):
+                window.close()
+                exit(0)
 
 
-
-        last_price = current_price
-        last_pos = current_pos
-        last_value = current_amount_value
-
-
-        if event == "currency_name+RIGHT CLICK+" or event == "price+RIGHT CLICK+" or event == "coinmarketcap_position+RIGHT CLICK+" or event == "current_amount_value+RIGHT CLICK+" or event == sg.WIN_CLOSED:
-            break
         event, values = window.read(timeout=refresh_rate)
 
     window.close()
